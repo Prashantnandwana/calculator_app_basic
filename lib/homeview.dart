f7fcbd89a7927b875b3c58a2620f0600e35e3f4d
F7 <7442774@gmail.com>
2023-10-28 12:10:27 +0530

    ic

---
+++ lib/homeview.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+      
+    class HomeView extends StatelessWidget {
+      const HomeView({super.key});
+    
+      @override
+      Widget build(BuildContext context) {
+        return Material(
+  child:   Container(
+  
+      color: Colors.indigo,
+  
+      child: Center(child: Text("hello world"),
+      ),
+  
+  ),
+);
+      }
+    }
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:calculator_app_basic/app.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
